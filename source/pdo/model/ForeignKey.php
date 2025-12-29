f61e9b6f6bd600711923d9d28d6ad997f72a7c81
4uruanna <adrien@villalonga.software>
2025-12-29 15:43:58 +0100

    feat: foreign key constraint

---
+++ source/pdo/model/ForeignKey.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Papimod\Database\pdo\model;
+
+class ForeignKey
+{
+    public function __construct(
+        public readonly string $column_name,
+        public readonly string $reference_table,
+        public readonly string $reference_column
+    ) {
+    }
+
+    public function __toString()
+    {
+        return <<<SQL
+                FOREIGN_KEY ({$this->column_name})
+                REFERENCES {$this->reference_table}({$this->reference_column})
+            SQL;
+    }
+}
