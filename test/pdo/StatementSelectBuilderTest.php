2d37550f5d8876f3355c4cd3b3d3470ccb06bbc8
4uruanna <adrien@villalonga.software>
2025-12-30 14:14:11 +0100

    test: select builder

---
+++ test/pdo/StatementSelectBuilderTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Papimod\Database\Test\pdo;
+
+use Faker\Factory;
+use Faker\Generator;
+use Papimod\Database\Database;
+use Papimod\Database\pdo\model\Column;
+use Papimod\Database\pdo\StatementSelectBuilder;
+use Papimod\Database\StatementBuilder;
+use PDO;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(StatementSelectBuilder::class)]
+final class StatementSelectBuilderTest extends TestCase
+{
+    private static Generator $faker;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$faker = Factory::create();
+    }
+
+    private string $table_name = "statement_select_builder_test";
+
+    private array $clients;
+
+    public function setUp(): void
+    {
+        $this->clients = [
+            [
+                "id" => 1,
+                "name" => self::$faker->userName(),
+                "age" => self::$faker->numberBetween(1, 100),
+                "last_connection" => "2020-11-19"
+            ],
+            [
+                "id" => 2,
+                "name" => self::$faker->userName(),
+                "age" => self::$faker->numberBetween(1, 100),
+                "last_connection" => "2021-11-19"
+            ],
+            [
+                "id" => 3,
+                "name" => self::$faker->userName(),
+                "age" => self::$faker->numberBetween(1, 100),
+                "last_connection" => "2022-11-19"
+            ],
+            [
+                "id" => 4,
+                "name" => self::$faker->userName(),
+                "age" => self::$faker->numberBetween(1, 100),
+                "last_connection" => "2023-11-19"
+            ]
+        ];
+
+        $pdo = Database::pdo();
+        $pdo->query("DROP TABLE IF EXISTS {$this->table_name}")->execute();
+        $pdo->query("CREATE TABLE IF NOT EXISTS {$this->table_name} (
+            `id` INT NOT NULL,
+            `name` TEXT NOT NULL,
+            `age` INT NOT NULL,
+            `last_connection` DATE NOT NULL,
+            PRIMARY KEY (`id`)
+        )")->execute();
+
+        foreach ($this->clients as $client) {
+            $pdo->prepare(
+                <<<SQL
+                    INSERT INTO {$this->table_name}
+                    (`id`, `name`, `age`, `last_connection`)
+                    VALUES
+                    (:_id, :_name, :_age, :_last_connection)
+                SQL
+            )
+                ->execute([
+                    ":_id" => $client["id"],
+                    ":_name" => $client["name"],
+                    ":_age" => $client["age"],
+                    ":_last_connection" => $client["last_connection"],
+                ]);
+        }
+    }
+
+    public function testSingle(): void
+    {
+        $statement = StatementBuilder::from($this->table_name)
+            ->select()
+            ->where("id")->isEqual(1)
+            ->build();
+
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        $this->assertEquals($this->clients[0]["name"], $result["name"]);
+    }
+
+    public function testMultiple(): void
+    {
+        $statement = StatementBuilder::from($this->table_name)
+            ->select()
+            ->where("id")->isGreaterOrEqual(2)
+            ->build();
+
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $this->assertEquals(3, count($result));
+    }
+
+    public function testSingleColumn(): void
+    {
+        $statement = StatementBuilder::from($this->table_name)
+            ->select(new Column("id"))
+            ->where("last_connection")->isLess("2021-11-19")
+            ->build();
+
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        $this->assertEquals(1, $result["id"]);
+        $this->assertFalse(isset($result["name"]));
+    }
+}
